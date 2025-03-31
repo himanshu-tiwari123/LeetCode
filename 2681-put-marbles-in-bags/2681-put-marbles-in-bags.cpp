85e02b1aa303a1391d2ce83829d7c1a5673d24ea
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-04-01 00:37:41 +0530

    Time: 5 ms (100%), Space: 61.4 MB (98.31%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -1,29 +1,20 @@
 class Solution {
     #define ll long long
 public:
-    long long putMarbles(vector<int>& weights, int k) {
-        int n = weights.size();
-        vector<ll>adjSums(n-1);
-
-        for(int i=0;i<n-1;i++){
-           adjSums[i]=weights[i]+weights[i+1];
-        }
-        // adjSums[n-1] = weights[0]+weights[n-1];
-
-        sort(adjSums.begin(),adjSums.end());
-
-        ll minSum =0,maxSum=0;
-
-        int i=0,j=n-2;
-        k--;
-
-        while(k--){
-            minSum += adjSums[i++];
-            maxSum += adjSums[j--];
-        }
-
-        return maxSum-minSum;
-
-        
+    long long putMarbles(vector<int>& wt, int k) {
+      if (--k == 0) return 0;
+        int n = wt.size() - 1;
+        for (int i = 0; i < n; ++i)
+            wt[i] += wt[i + 1];
+        wt.pop_back();
+        long ans = 0;
+        nth_element(wt.begin(), wt.begin() + k, wt.end());
+        for (int i = 0; i < k; ++i)
+            ans -= wt[i];
+        nth_element(wt.begin(), wt.end() - k, wt.end());
+        for (int i = 0; i < k; ++i)
+            ans += wt[n - 1 - i];
+        return ans;
     }
+    
 };
