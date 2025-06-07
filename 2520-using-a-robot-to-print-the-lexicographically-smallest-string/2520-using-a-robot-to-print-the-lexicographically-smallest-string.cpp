7e4351ec4fb79722969f59fadebf36881e3a19f6
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-06-07 13:16:30 +0530

    Time: 44 ms (74.26%), Space: 35 MB (29.54%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -2,33 +2,32 @@ class Solution {
 public:
     string robotWithString(string s) {
         int n = s.size();
-        // 1) Build a "minSuffix" array so that minSuffix[i] = smallest character in s[i..n-1].
-        vector<char> minSuffix(n);
+        vector<char>minSuffix(n);
+        
         minSuffix[n-1] = s[n-1];
-        for (int i = n-2; i >= 0; --i) {
-            // take the smaller of s[i] or the suffix-min at i+1
-            minSuffix[i] = min(s[i], minSuffix[i+1]);
+        for(int i=n-2;i>=0;i--){
+            minSuffix[i] = min(minSuffix[i+1],s[i]);
         }
+        
+        stack<char>st;
+        string ans="";
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
 
-        string ans;
-        ans.reserve(n);
-        stack<char> st;     // this represents the robot’s string t
-        int i = 0;
+            char nextMin = (i+1 < n? minSuffix[i+1] : char('z'+1));
 
-        // 2) While there are still characters left in s (i < n) or the stack isn’t empty:
-        while (i < n || !st.empty()) {
-            // If the stack is nonempty AND (we’ve used up all of s OR top of stack ≤ smallest remaining in s),
-            // then pop from stack into ans.
-            if (!st.empty() && (i == n || st.top() <= minSuffix[i])) {
-                ans.push_back(st.top());
+            while(!st.empty() and st.top() <= nextMin){
+                ans += st.top();
                 st.pop();
-            } else {
-                // Otherwise, we still need to take the next character from s[i] and push it into the stack.
-                st.push(s[i]);
-                ++i;
             }
         }
 
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+
         return ans;
+
     }
 };
