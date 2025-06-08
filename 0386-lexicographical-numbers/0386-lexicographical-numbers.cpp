06a90cf4378882fdce9dbeb9356f0a7efd4c36d7
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-06-08 12:53:49 +0530

    Time: 3 ms (45.99%), Space: 15.1 MB (26.31%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    vector<int>ans;
+    void recursion(int curr_val,int n){
+      
+        //Base Case:
+        if(curr_val > n){
+            return;
+        }
+        ans.push_back(curr_val);
+
+        for(int i=0;i<=9;i++){
+            int next_val = curr_val*10+i;
+
+            if(next_val <= n){
+                recursion(next_val,n);
+            }
+        }
+    }
+public:
+    vector<int> lexicalOrder(int n) {
+
+        for(int val=1;val<=9;val++){
+            recursion(val,n);
+        }
+        
+
+        return ans;
+    }
+};
