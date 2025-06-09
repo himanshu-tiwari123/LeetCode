327699b994bb9d5668495afac9278f7c514baff8
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-06-09 18:32:30 +0530

    Time: 18 ms (83.33%), Space: 17.6 MB (22.06%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,45 +1,68 @@
 class Solution {
-    #define pb push_back
-    #define ll long long
+#define pb push_back
+#define ll long long
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        
-        //We will be HashMap pointers man:
-        set<vector<int>>st;
-        
-        //We will store elemnts between pointers j and k in a hashSet, to
-        // prevent dupicate elements ie. i!=j and j!=k and k!=l and l!=i;
+        vector<vector<int>> res;
+        // We will be HashMap pointers man:
+        set<vector<int>> st;
 
-        for(int i=0;i<n-2;i++){
+        sort(nums.begin(), nums.end());
 
-            for(int j=i+1;j<n-1;j++){
+        // We will store elemnts between pointers j and k in a hashSet, to
+        //  prevent dupicate elements ie. i!=j and j!=k and k!=l and l!=i;
 
-                unordered_set<int>freq;
+        // Now for optimal one we will use a two pointer approach.Here we will
+        // use
+        //  two loops i and j for traversal and then do the remaining part using
+        //  two-pointer approach.Also we try to skip similar elements to avoid
+        //  duplicates:
 
-                for(int k=j+1;k<n;k++){
+        for (int i = 0; i < n - 3; i++) {
 
-                    ll curr_sum =  nums[i]+nums[j];
+            if (i > 0 and nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if (j > i + 1 and nums[j] == nums[j - 1])
+                    continue;
+
+                int k = j + 1, l = n - 1;
+
+                while (k < l) {
+
+                    ll curr_sum = nums[i] + nums[j];
                     curr_sum += nums[k];
+                    curr_sum += nums[l];
 
-                    ll required_sum = target-curr_sum;
+                    // cout << curr_sum << " ";
 
-                    // cout<<curr_sum<<" "<<required_sum<<"\n";
+                    if (curr_sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        res.push_back(temp);
+                        k++;l--;
+                        if (k > j + 1) {
+                            while (k < l and nums[k] == nums[k - 1]) {
+                                k++;
+                            }
+                        }
+                        if (l < n - 1) {
+                            while (l > k and nums[l] == nums[l + 1]) {
+                                l--;
+                            }
+                        }
 
-                    if(freq.find(required_sum)!=freq.end() and required_sum>=INT_MIN and required_sum<=INT_MAX){
-                        vector<int>ans = {nums[i],nums[j],nums[k],(int)required_sum};
-                        sort(ans.begin(),ans.end());
-                        st.insert(ans);
+                    } else if (curr_sum < target) {
+                        k++;
+                    } else {
+                        l--;
                     }
-
-                    freq.insert(nums[k]);
-
                 }
             }
         }
 
-        vector<vector<int>>res(st.begin(),st.end());
-
-       return res;
+        return res;
     }
 };
