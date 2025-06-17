a33d371d9560ce70564e7b9f08800c2065156aa1
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-06-17 16:21:13 +0530

    Time: 0 ms (100%), Space: 16.7 MB (56.67%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.cpp
+++ 1105-uncrossed-lines/1105-uncrossed-lines.cpp
@@ -5,9 +5,25 @@ public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int  n = nums1.size(),m = nums2.size();
 
-        dp.assign(n+1,vector<int>(m+1,-1));
+        // dp.assign(n+1,vector<int>(m+1,-1));
 
-        return findAns(0,0,nums1,nums2);
+        // return findAns(0,0,nums1,nums2);
+
+        dp.assign(n+1,vector<int>(m+1,0));
+        //Bottom - up approach:
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }else{
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+
+
     }
 private:
     int findAns(int i,int j,vector<int>&nums1,vector<int>&nums2){
