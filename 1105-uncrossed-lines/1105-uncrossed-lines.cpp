da660b46e3b4783b9984dbad8b4cbf6a51d564e5
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-06-17 15:51:05 +0530

    Time: 7 ms (55.61%), Space: 16.8 MB (16.95%) - LeetHub

---
+++ 1105-uncrossed-lines/1105-uncrossed-lines.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    vector<vector<int>>dp;
+public:
+     
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int  n = nums1.size(),m = nums2.size();
+
+        dp.assign(n+1,vector<int>(m+1,-1));
+
+        return findAns(0,0,nums1,nums2);
+    }
+private:
+    int findAns(int i,int j,vector<int>&nums1,vector<int>&nums2){
+        //Base Case:
+        if(i>=nums1.size() or j>=nums2.size()){
+            return 0;
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int connect=0;
+
+        if(nums1[i] == nums2[j]){
+            connect = 1 + findAns(i+1,j+1,nums1,nums2);
+        }
+        
+        int not_connect = max(findAns(i+1,j,nums1,nums2),findAns(i,j+1,nums1,nums2));
+
+
+        return dp[i][j] = max(connect,not_connect);
+    }
+};
