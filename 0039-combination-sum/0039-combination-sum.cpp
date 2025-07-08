cbb2c16995f44fd596e040f5ae6b0b1b6cb956f9
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-07-08 18:51:30 +0530

    Time: 8 ms (29.98%), Space: 18.2 MB (27.13%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<int>>ans;
+    void findAns(int index,vector<int>& candidates,vector<int>&res,int target){
+        //Base Case:
+        if(index>=candidates.size()){
+            return;
+        }
+        if(target == 0){
+            ans.push_back(res);
+            return;
+        }
+
+        findAns(index+1,candidates,res,target);
+
+        if(target-candidates[index] >= 0){
+            res.push_back(candidates[index]);
+            findAns(index,candidates,res,target-candidates[index]);
+            res.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>res;
+        findAns(0,candidates,res,target);
+        return ans;
+    }
+};
