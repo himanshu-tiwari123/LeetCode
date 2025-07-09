51a25aab751810659bf7775f05ff8901d1aea4ea
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-07-09 16:34:29 +0530

    Time: 73 ms (22.58%), Space: 84 MB (21.82%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    vector<vector<string>>ans;
+
+    bool isPalindrome(string &s){
+        if(s.size()==1 ) return true;
+        int l=0,r=s.size()-1;
+        while(l<=r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++;r--;
+        }
+
+        return true;
+    }
+    
+    void findAns(int index,string str,vector<string>&temp,string s){
+        //Base Case:
+        if(index>=s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=index;i<s.size();i++){
+            string str = s.substr(index , i-index+1);
+
+            if(isPalindrome(str)){
+                temp.push_back(str);
+
+                findAns(i+1,str,temp,s);
+
+                temp.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string>temp;
+        findAns(0,"",temp,s);
+        return ans;
+    }
+};
