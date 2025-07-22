85573732d22c67e2ac2680a37d6b072c4f99e2c3
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-07-22 23:08:23 +0530

    Time: 3 ms (30.19%), Space: 95 MB (78.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,40 +2,69 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(),m = nums2.size();
-        vector<int>arr;
+        // vector<int>arr;
         int i=0,j=0;
+        int index=0,val1=-1,val2=-1;
+        int k = ((m+n)/2)-1;
         while(i<n and j<m){
+
+            if(val1>=0 and val2>=0){
+                break;
+            }
+            
             if(nums1[i] <= nums2[j]){
-                arr.push_back(nums1[i]);
+                if(index==k){
+                    val1 = nums1[i];
+                }
+                if(index==k+1){
+                    val2 = nums1[i];
+                }
+                
                 i++;
             }else{
-                arr.push_back(nums2[j]);
+                if(index==k){
+                    val1 = nums2[j];
+                }
+                if(index==k+1){
+                    val2 = nums2[j];
+                }
+                
                 j++;
             }
+            index++;
         }
 
         while(i<n){
-            arr.push_back(nums1[i]);
+            if(index==k){
+                val1 = nums1[i];
+            }
+            if(index==k+1){
+                val2 = nums1[i];
+            }
+            index++;
             i++;
         }
 
         while(j<m){
-            arr.push_back(nums2[j]);
+            if(index==k){
+                val1 = nums2[j];
+            }
+            if(index==k+1){
+                val2 = nums2[j];
+            }
+            index++;
             j++;
         }
 
-        int len = arr.size();
-
         double ans = 0.0;
 
-        if(len&1){
-            //length is odd:
-            ans =(double)arr[len/2];
+        if((n+m)&1){
+            //its odd:
+            ans = val2;
         }else{
-            ans = (arr[(len/2)]+arr[(len/2)-1])/2.0;
+            ans = (val1 + val2)/2.0;
         }
-
-        return ans;
+        return  ans;
 
     }
 };
