900695b5992d5c2645056de8cabf665e2ccaebe7
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-07-28 13:19:03 +0530

    Time: 3 ms (6.67%), Space: 13.3 MB (3.7%) - LeetHub

---
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>left(n),right(n);
+
+        deque<int>dq;
+        dq.push_front(nums[0]);
+        
+        for(int i=0;i<n;i++){
+           int val = nums[i];
+
+            while(i<n and nums[i]==val){
+               if(nums[i] == dq.front()){
+                  left[i] = -1;
+               }else{
+                left[i] = dq.front();
+               }
+
+               i++;
+            }
+            i--;
+
+           dq.push_front(nums[i]);
+        }
+
+        dq.push_front(nums[n-1]);
+
+        for(int i=n-1;i>=0;i--){
+           int val = nums[i];
+
+            while(i>=0 and nums[i]==val){
+               if(nums[i] == dq.front()){
+                  right[i] = -1;
+               }else{
+                right[i] = dq.front();
+               }
+
+               i--;
+            }
+            i++;
+
+           dq.push_front(nums[i]);
+        }
+        int hills = 0,valleys = 0;
+        for(int i=1;i<=n-2;i++){
+            if(i>1 and nums[i]== nums[i-1]){
+                continue;
+            }
+            if(left[i]!=-1 and right[i]!=-1){
+               if(nums[i] > left[i] and nums[i]>right[i]){
+                hills++;
+               }else if(nums[i] < left[i] and nums[i]<right[i]){
+                valleys++;
+               }
+            }
+        }
+
+        return hills + valleys;
+    }
+};
