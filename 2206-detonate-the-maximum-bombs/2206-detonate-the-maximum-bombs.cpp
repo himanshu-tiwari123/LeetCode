e4b136f56292d0c4607f3cb24d384f5c5ff3a0dd
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-10 15:57:20 +0530

    Time: 47 ms (74.81%), Space: 19.4 MB (66.37%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,46 @@
+#define ll long long
+
+class Solution {
+    int dfs(int node,vector<bool>&visited,unordered_map<int,vector<int>>&adj){
+        visited[node] = true;
+        int cnt = 1;
+
+        for(auto &child : adj[node]){
+            if(!visited[child]){
+                cnt += dfs(child,visited,adj);
+            }
+        }
+
+        return cnt;
+    }
+public:
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+
+        unordered_map<int,vector<int>>adj;
+
+        for(int i=0;i<n;i++){
+           ll x1=bombs[i][0],y1=bombs[i][1],r=bombs[i][2];
+            for(int j=0;j<n ;j++){
+                if(i==j) continue;
+               ll x2 =bombs[j][0],y2=bombs[j][1];
+
+               ll dx= (x2-x1)*(x2-x1);
+               ll dy = (y2-y1)*(y2-y1);
+
+            //    ll dist = sqrt(dx+dy);
+               if(dx+dy <= r*r){
+                adj[i].push_back(j);
+               }
+            }
+        }
+        int max_cnt =0;
+        for(int i=0;i<n;i++){
+            vector<bool>visited(n+1,false);
+            // int cnt = 0;
+            max_cnt = max(max_cnt,dfs(i,visited,adj));
+        }
+
+        return max_cnt;
+    }
+};
