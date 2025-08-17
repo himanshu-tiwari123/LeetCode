f855a5748266664908cb614add9554538a50b073
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-17 15:30:12 +0530

    Time: 14 ms (66.59%), Space: 328.2 MB (54.45%) - LeetHub

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -1,42 +1,51 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
+    #define ll long long
 public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-        int n = (int)prices.size();
-        long long original = 0;
-        for (int i = 0; i < n; ++i) original += (long long)strategy[i] * prices[i];
+        int n = prices.size();
+        vector<ll>leftDelta(n) , rightDelta(n);
 
-        int half = k / 2;
+        ll originalSum = 0;
 
-        
-        vector<long long> leftDelta(n), rightDelta(n);
-        for (int i = 0; i < n; ++i) {
-            leftDelta[i]  = - (long long)strategy[i] * (long long)prices[i];
-            rightDelta[i] = (1LL - (long long)strategy[i]) * (long long)prices[i];   
+        for(int i=0;i<n;i++){
+            originalSum += (strategy[i] * prices[i]);
         }
 
-        // initial window s = 0
-        long long leftSum = 0, rightSum = 0;
-        for (int i = 0; i < half; ++i) leftSum += leftDelta[i];
-        for (int i = half; i < k; ++i) rightSum += rightDelta[i];
-
-        long long best = max(original, original + leftSum + rightSum);
-
-        
-        for (int s = 1; s + k - 1 < n; ++s) {
-
-            leftSum  -= leftDelta[s - 1];
-            leftSum  += leftDelta[s + half - 1];
-
-            rightSum -= rightDelta[s + half - 1];
-            rightSum += rightDelta[s + k - 1];
-
-            best = max(best, original + leftSum + rightSum);
+        for(int i=0;i<n;i++){
+            leftDelta[i] = (0-strategy[i]) * prices[i];
+            rightDelta[i] = (1-strategy[i]) * prices[i];
         }
 
-        return best;
+        ll leftSum = 0,rightSum =0;
+
+        for(int i=0;i<(k/2);i++){
+            leftSum += leftDelta[i];
+        }
+
+        for(int i=(k/2);i<(k);i++){
+            rightSum += rightDelta[i];
+        }
+        
+
+        ll maxSum = originalSum;
+
+        maxSum = max(maxSum , originalSum + rightSum + leftSum);
+
+        int l=1,r=k;
+
+        while(r<n){
+            leftSum -= leftDelta[l-1];
+            leftSum += leftDelta[l+(k/2)-1];
+
+            rightSum -= rightDelta[l+(k/2)-1];
+            rightSum += rightDelta[r];
+
+            l++;
+            r++;
+
+            maxSum = max(maxSum , originalSum + leftSum + rightSum);
+        }
+
+        return maxSum;
     }
 };
