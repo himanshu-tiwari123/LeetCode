beffc5a7271f13dd98bdee6518efa46a4316cb10
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-20 00:44:53 +0530

    Time: 0 ms (100%), Space: 22.9 MB (61.82%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    #define ll long long
+public:
+    int candy(vector<int>& ratings) {
+        int  n = ratings.size();
+
+        vector<int>ans(n,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                ans[i] = (ans[i-1]+1);
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                ans[i] = max(ans[i] , ans[i+1]+1);
+            }
+        }
+
+        ll res = accumulate(ans.begin(),ans.end(),(ll)0);
+
+        return res;
+    }
+};
