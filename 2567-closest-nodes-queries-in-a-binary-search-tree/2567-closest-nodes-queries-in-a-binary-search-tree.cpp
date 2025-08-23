8b72b1207a12eb5d066d50908577df3d87c5bb1b
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-23 23:57:17 +0530

    Time: 114 ms (14.8%), Space: 205.1 MB (15.89%) - LeetHub

---
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/2567-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int>sorted;
+
+    int binarySearch1(vector<int>&arr, int val){
+        int l=0,h=arr.size()-1;
+        int ans = -1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+
+            if(arr[mid] <= val){
+              l = mid+1;
+              ans = arr[mid];
+            }else{
+                h = mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    int binarySearch2(vector<int>&arr, int val){
+        int l=0,h=arr.size()-1;
+        int ans = -1;
+        while(l<=h){
+            int mid = l+ (h-l)/2;
+
+            if(arr[mid] >= val){
+              h = mid-1;
+              ans = arr[mid];
+            }else{
+                l = mid+1;
+            }
+        }
+
+        return ans;
+    }
+
+    void dfs(TreeNode *node){
+        if(node == NULL){
+            return;
+        }
+
+        dfs(node->left);
+
+        sorted.push_back(node->val);
+
+        dfs(node->right);
+    }
+public:
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+        vector<vector<int>>ans;
+        
+        dfs(root);
+
+        for(auto &it: queries){
+            int a = binarySearch1(sorted,it);
+
+            int b = binarySearch2(sorted,it);
+
+            vector<int>temp = {a,b};
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
