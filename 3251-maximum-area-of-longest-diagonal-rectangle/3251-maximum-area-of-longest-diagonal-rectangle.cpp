338f0dbfe2f4ba676efbb368be6a65c1d67dbb4f
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-27 00:06:12 +0530

    Time: 0 ms (100%), Space: 29.4 MB (52.12%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -3,31 +3,28 @@ public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
         int max_area = 0;
 
-        double diagonal = 0.0;
+        int diagonal = 0;
 
-        vector<pair<double,int>>v;
 
         for(auto &it: dimensions){
             int a=it[0],b=it[1];
 
-            double curr_diagonal = (double)sqrt((a*a) + (b*b));
-            
+            int curr_diagonal = a*a + b*b;
+
             int area = a*b;
 
-            v.push_back({curr_diagonal,area});
+            if(curr_diagonal == diagonal){
+                if(a*b > max_area){
+                    max_area =a*b;
+                }
+
+            }else if(curr_diagonal > diagonal){
+                diagonal = curr_diagonal;
+                max_area = area;
+            }
             
         }
 
-        auto compare = [&](pair<double,int>a,pair<double,int>b){
-            if(a.first == b.first){
-                return a.second > b.second;
-            }
-
-            return a.first > b.first;
-        };
-
-        sort(v.begin(),v.end(),compare);
-
-        return v[0].second;
+       return max_area;
     }
 };
