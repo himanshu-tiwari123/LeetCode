fd96f699c84a54cdf386a8ad8cd6e52c5cab18ee
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-08-28 21:10:19 +0530

    Time: 83 ms (26.87%), Space: 286.8 MB (18.95%) - LeetHub

---
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        int n=nums.size();
+
+        if(n%k != 0) return false;
+
+        map<int,int>mp;
+
+        for(auto &it: nums){
+            mp[it]++;
+        }
+
+        
+        for(auto &it: mp){
+            if(it.second > (n/k)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
