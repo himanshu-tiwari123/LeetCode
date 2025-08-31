16aa666e495ea5c4985e8574e1a979ea529b2168
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-09-01 00:15:13 +0530

    Time: 332 ms (57.63%), Space: 8.9 MB (8.17%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,61 +1,62 @@
 class Solution {
-    int n;
-    vector<vector<char>>ans;
-    bool canPlace(int row,int col,char val,vector<vector<char>>& board){
-        
-        for(int i=0;i<9;i++){
-            if(board[row][i] == val) return false;
+    bool isValid(int i,int j,int val,vector<vector<char>>& board){
+    
+        for(int l=0;l<9;l++){
+            if(board[i][l]==(val+'0')){
+                return false;
+            }
 
-            if(board[i][col] == val) return false;
+            if(board[l][j] == (val+'0')) return false;
         }
-        
 
-        int row1=row/3,col1=col/3;
+        // unordered_map<int,pair<int,int>>a,b;
+        // a[0]={0,2},a[1]={3,5},a[2] = {6,8};
+        // b[0]={0,2},b[1]={3,5},b[2] = {6,8};
+        // int x = i/3,y=j/3;
 
-        row1 *=3,col1*=3;
+        // int l1,r1,l2,r2;
+        // l1=a[x].first;r1=b[y].first;
+        // l2=a[x].second;r2 = b[y].second;
 
-        for(int i=row1;i<row1+3;i++){
-            for(int j=col1;j<col1+3;j++){
-                if(board[i][j] == val){
+        // for(int o=l1;o<=l2;o++){
+        //     for(int p=r1;p<=r2;p++){
+        //         if(board[o][p] == (val+'0')){
+        //             return false;
+        //         }
+        //     }
+        // }
+        int row = (i/3)*3,col = (j/3)*3;
+
+        for(int k=row;k<row+3;k++){
+            for(int l=col;l<col+3;l++){
+                if(board[k][l] == val+'0') return false;
+            }
+        }
+        return true;
+    }
+    bool findAns(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(int val=1;val<=9;val++){
+                        if(isValid(i,j,val,board)){
+                            board[i][j] = val+'0';
+                            if(findAns(board)) return true;
+
+                            board[i][j] = '.';
+                        }
+                    }
+
                     return false;
                 }
             }
-        } 
+        }
 
         return true;
-
     }
-
-    bool findAns(int row,int col,vector<vector<char>>& board){
-        if(row >= n){
-           return true;
-        }
-        if(col>=n){
-            return findAns(row+1,0,board);
-        }
-        if(board[row][col]!='.'){
-            return findAns(row,col+1,board);
-        }
-
-        for(char val='1';val<='9';val++){
-           if(canPlace(row,col,val,board)){
-                board[row][col] =val;
-
-                if(findAns(row,col+1,board)) return true;
-
-                board[row][col] ='.';
-
-           }
-        }
-
-        return false;
-    }
-
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        n = board.size();
-       
-       findAns(0,0,board);
+        findAns(board);
 
     }
-};
+};  
