6c4af0988537564498e709559e21e48743a5d93c
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-09-11 00:53:39 +0530

    Time: 14 ms (5.28%), Space: 24.4 MB (100%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int leftMax = 0,rightMax = 0;
-        int l = 0,r = n-1;
-
-        int ans = 0;
-
-        while(l<=r){
-            if(height[l] <= height[r]){
-                if(height[l] >= leftMax){
-                    leftMax = height[l];
-                }else{
-                    ans += leftMax-height[l];
-                }
-                l++;
-            }else{
-                if(height[r] >= rightMax){
-                    rightMax = height[r];
-                }else{
-                    ans += rightMax - height[r];
-                }
-                r--;
-            }
+        int n=height.size();int max_left=0,max_right=0;
+        vector<int>prefix_left;vector<int>suffix_right;
+     //Creating prefix-left ;
+        for(int i=0;i<n;i++){
+        max_left=max( max_left,height[i]);
+        prefix_left.push_back(max_left);
         }
+    //Creating suffix
+        for(int i=n-1;i>=0;i--){
+        max_right=max( max_right,height[i]);
+        suffix_right.push_back(max_right);
+        }
+       //Reverse the max_right
+       reverse(suffix_right.begin(),suffix_right.end());
+       //Calculating the amount of water that can be stored
+       int  sum=0;
 
-        return ans;
+       for(int i=0;i<n;i++){
+           int amt_of_water_filled=min(prefix_left[i],suffix_right[i])-height[i];
+           sum +=amt_of_water_filled;
+       }
+       return sum;
 
     }
 };
