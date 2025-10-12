96b7d4de51ba4698bada200935541243ea2166e5
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-13 00:52:35 +0530

    Time: 10 ms (42.42%), Space: 14 MB (55%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,16 +1,9 @@
 class Solution {
-
-    bool isValid(int i,int j,vector<vector<int>>& grid){
-        int n = grid.size();
-
-        return i>=0 and i<n and j>=0 and j<n;
-    }
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        int n=grid.size();
 
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
 
         vector<vector<int>>distance(n,vector<int>(n,1e9));
 
@@ -20,31 +13,30 @@ public:
 
         vector<pair<int,int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
 
-        // int curr_time = INT_MIN;
+        auto isValid = [](int i,int j,vector<vector<int>>&grid){
+            return i>=0 and i<grid.size() and j>=0 and j<grid.size();
+        };
+
 
         while(!pq.empty()){
-            auto [t,i,j] = pq.top();
-            pq.pop();
+            auto [t,i,j] = pq.top();pq.pop();
+            
+            if(i==n-1 and j==n-1) return t;
 
-            if(i==n-1 and j==n-1){
-                return t;
-            }
-            
-            
             for(auto &it: directions){
                 int x = i + it.first;
                 int y = j + it.second;
 
                 if(isValid(x,y,grid)){
-                    int curr_time = max(t,grid[x][y]);
-                    
-                    if(curr_time < distance[x][y]){
-                        distance[x][y] = curr_time;
+                   int newTime = max(t,grid[x][y]);
 
-                        pq.push({distance[x][y],x,y});
-                    }
+                   if(newTime < distance[x][y]){
+                    distance[x][y] = newTime;
+                    pq.push({distance[x][y] , x,y});
+                   }
                 }
             }
+
         }
 
         return distance[n-1][n-1];
