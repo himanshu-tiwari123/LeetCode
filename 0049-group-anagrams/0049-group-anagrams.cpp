18fc41a8c7a44c25f8870b4a2493ea9279cdb4a5
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-13 15:09:48 +0530

    Time: 22 ms (35.34%), Space: 28.5 MB (21.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,25 +3,34 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
 
-        map<string,vector<int>>mp;
+        map<string,vector<string>>mp;
 
         for(int i=0;i<strs.size();i++){
             string temp = strs[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(i);
-        }
 
-        for(auto &v: mp){
-            vector<string>res;
-             
-            for(auto &i: v.second){
-                res.push_back(strs[i]);
+            vector<int>freq(26,0);
+
+            for(int i=0;i<temp.size();i++){
+                freq[temp[i]-'a']++;
+            }
+            string curr = "";
+            for(int i=0;i<26;i++){
+                while(freq[i]>0){
+                    curr += (i+'a');
+                    freq[i]--;
+                }
             }
 
-            ans.push_back(res);
+            mp[curr].push_back(strs[i]);
+        }
+
+        for(auto &it: mp){
+            ans.push_back(it.second);
         }
 
 
         return ans;
+
+        
     }
 };
