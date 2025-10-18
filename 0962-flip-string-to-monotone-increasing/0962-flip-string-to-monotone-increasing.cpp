1c467563b62d5a7bc57d58f93a40cb2a2a86ab01
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-18 23:15:31 +0530

    Time: 8 ms (61.83%), Space: 13.5 MB (87.83%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
@@ -2,30 +2,16 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        vector<int>prefix(n,0),suffix(n,0);
-        //we use prefix to calculate the no of 1s and suffix to calculate the no of zeros:
-        prefix[0] = (s[0]=='1' ? 1: 0);
-        suffix[n-1] = (s[n-1] == '0' ? 1: 0);
+        int ones_cnt = 0,flips = 0;
 
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] + (s[i] == '1' ? 1:0);
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                ones_cnt++;
+            }else{
+                flips = min(flips+1,ones_cnt);
+            }
         }
 
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1] + (s[i] == '0' ? 1: 0);
-        }
-
-       
-        int min_flips = n;
-
-        for(int i=0;i<n-1;i++){
-            int curr_flips = prefix[i] + suffix[i+1];
-            min_flips = min(min_flips,curr_flips);
-        }
-
-        min_flips = min(min_flips,prefix[n-1]);
-        min_flips = min(min_flips,suffix[0]);
-
-        return min_flips;
+        return flips;
     }
 };
