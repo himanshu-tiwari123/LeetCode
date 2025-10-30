6509489924b885841e2e080ce541ecb096945d8b
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-30 20:16:30 +0530

    Time: 109 ms (11.18%), Space: 18.7 MB (68.5%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,16 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size() , m = matrix[0].size();
 
-        // we need to use some kind of logic for this man,but I am unable to get how to do this man!!!
-
         int l = 0,r = (m*n)-1;
 
         for(int i=0;i<n;i++){
             auto lb = lower_bound(matrix[i].begin(),matrix[i].end(),target)-matrix[i].begin();
-            if((lb < m ) and (matrix[i][lb] == target) ){
+
+            if(lb< m and matrix[i][lb] == target){
                 return true;
             }
         }
+
+        //The TC is O(n * log(m));
 
         return false;
     }
