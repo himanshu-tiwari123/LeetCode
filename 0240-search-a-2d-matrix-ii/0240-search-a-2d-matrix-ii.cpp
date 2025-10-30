38bea5da91ea151f8e06ad07324a411d2027d292
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-30 20:12:36 +0530

    Time: 110 ms (10.92%), Space: 18.8 MB (37.55%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,32 +1,19 @@
 class Solution {
-    bool binarySearch(int i,int target,vector<vector<int>>& matrix){
-        int low = 0,high = matrix[0].size();
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(matrix[i][mid] == target){
-                return true;
-            }
-            else if(matrix[i][mid] > target){
-                high = mid-1;
-            }else{
-                low =  mid+1;
-            }
-        }
-
-      return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(),m = matrix[0].size();
+        int n = matrix.size() , m = matrix[0].size();
 
-        bool ans = false;
+        // we need to use some kind of logic for this man,but I am unable to get how to do this man!!!
+
+        int l = 0,r = (m*n)-1;
 
         for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target and target<=matrix[i][m-1]){
-                ans |= binarySearch(i,target,matrix);
+            auto lb = lower_bound(matrix[i].begin(),matrix[i].end(),target)-matrix[i].begin();
+            if((lb < m ) and (matrix[i][lb] == target) ){
+                return true;
             }
         }
 
-        return ans;
+        return false;
     }
 };
