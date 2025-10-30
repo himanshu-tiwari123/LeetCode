8bd87aab974c0b1b2961feb8bd59297693f7657b
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-30 20:32:31 +0530

    Time: 55 ms (50.56%), Space: 18.8 MB (37.55%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,17 +3,25 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size() , m = matrix[0].size();
 
-        int l = 0,r = (m*n)-1;
+        int l = 0,r = m-1;
 
-        for(int i=0;i<n;i++){
-            auto lb = lower_bound(matrix[i].begin(),matrix[i].end(),target)-matrix[i].begin();
+        if(matrix[l][r] == target) return true;
 
-            if(lb< m and matrix[i][lb] == target){
+        //I am starting from top-right cell of the matrix.If the values are less so we move towards left else if the values are bigger we move down.If we go out of bounds we are done,we were not able to find this else if we found we return true:
+
+        //note we have placed l as 0 so it will move downward:
+
+        while(l<n and r>=0){
+            if(matrix[l][r] == target){
                 return true;
+            }else if(matrix[l][r] > target){
+                r--; // we move towrds left;
+            }else {
+                l++;
             }
         }
 
-        //The TC is O(n * log(m));
+        
 
         return false;
     }
