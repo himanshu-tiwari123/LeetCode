2e2163fbb0755f5279d668059408ef032424da19
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-30 19:53:12 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,38 +4,25 @@ public:
         //we need to use binary Search man:
 
         int n =  matrix.size(), m= matrix[0].size();
+        
+        int l=0,r=(m*n)-1;
 
-        //we can think of serializing the matrix man;
+        while(l<=r){
+            int mid = l + (r-l)/2;
 
-        vector<int>arr;
+            int i = (mid/m) ,j = (mid%m);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-               arr.push_back(matrix[i][j]);
-
-
-            }
-        }
-
-        //Now lets apply Bs;
-        int low=0,high = arr.size()-1;
-
-        while(low<= high){
-            int mid = low + (high-low)/2;
-
-            if(arr[mid] == target){
+            if(matrix[i][j] == target){
                 return true;
-            }else if(arr[mid] < target){
-                low = mid+1;
-            }else
-            {
-                high = mid-1;
+            }else if(matrix[i][j] < target){
+                l = mid+1;
+            }else{
+                r = mid-1;
             }
-        }
 
+        }
 
         return false;
-
 
     }
 };
