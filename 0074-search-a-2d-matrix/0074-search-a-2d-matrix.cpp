88decbe2ee823c03942f0eb1a9a528cb5b0e11a2
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2025-10-30 19:39:14 +0530

    Time: 0 ms (100%), Space: 13.7 MB (6.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int  n = matrix.size(),m=matrix[0].size();
-        int low = 0,high = m*n-1;
+        //we need to use binary Search man:
 
-        while(low<=high){
-            int mid = low+(high-low)/2;
+        int n =  matrix.size(), m= matrix[0].size();
 
-            //Now we need to find row and col of mid:
-            int row = mid/m, col = mid%m;
+        //we can think of serializing the matrix man;
 
-            if(matrix[row][col] == target){
-                return true;
-            }
-            else if(matrix[row][col]  > target){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+        vector<int>arr;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               arr.push_back(matrix[i][j]);
+
+
             }
         }
 
+        //Now lets apply Bs;
+        int low=0,high = arr.size()-1;
+
+        while(low<= high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid] == target){
+                return true;
+            }else if(arr[mid] < target){
+                low = mid+1;
+            }else
+            {
+                high = mid-1;
+            }
+        }
+
+
         return false;
+
+
     }
 };
