c5a91ab3853e8f3fb2032e598ec9d21854e0a376
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-05-01 13:19:08 +0530

    Time: 2735 ms (5.07%), Space: 150.2 MB (88.31%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    int dp[201][201][501];
+    int n, m;
+
+
+    int findAns(int i,int j,int k,vector<vector<int>>& grid){
+
+        //base Case:
+        if(i>=n or j>=m ){
+            return -1e9;
+        }
+        
+        int val = grid[i][j];
+
+        if(val == 2){
+            val = 1;
+        }
+
+        if(i==n-1 and j==m-1){
+            if(k-val >=0 ){
+                return grid[i][j];
+            }else{
+                return -1e9;
+            }
+        }
+
+        if(dp[i][j][k] != -1){
+            return dp[i][j][k];
+        }
+
+        int right = -1e9, down = -1e9;
+
+
+        if(k-val >= 0){
+            right = grid[i][j] + findAns(i+1, j, k-val,grid);
+             
+            down = grid[i][j] + findAns(i,j+1,k-val,grid);
+        }
+
+        return dp[i][j][k] = max(right, down);
+        
+    }
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        n = grid.size(); m = grid[0].size();
+         
+        memset(dp,-1, sizeof(dp));
+
+        if(k>500){
+            k=500;
+        }
+
+        int ans = findAns(0,0,k,grid);
+
+        return (ans < 0 ? -1 : ans);
+
+          
+    }
+};
