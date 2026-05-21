32567cec795df92f87d335329d2090af62a4df59
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-05-22 01:57:00 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //so to search in a rotated sorted array we will keep track of arr[mid] and arr[high]
-        //if(arr[mid] > arr[high])=>that part from mid to high is not sorted ,so pivot will be      
-        // here and therefore we do low = mid+1 else we do high = mid-1.As soon as arr[mid]==target we 
-        // return  it:
-
         int n = nums.size();
+        int left=0,right=n-1;
 
-        int low = 0,high = n-1;
+        
 
-        while(low<=high){
-            int mid = low + (high-low)/2;
+        while(left<=right){
+            int mid = left+(right-left)/2;
 
             if(nums[mid] == target){
                 return mid;
             }
-
-            //Left part of array is sorted:
-            if(nums[mid] > nums[high]){
-                //check if targer is present here:
-                if(nums[low] <= target and target<=nums[mid]){
-                    high  = mid-1;
+            if(nums[left] <= nums[mid]){
+                if(nums[left]<=target and target<=nums[mid]){
+                    right = mid-1;
                 }else{
-                    low = mid+1;
+                    left = mid+1;
                 }
             }else{
-                //right part is sorted:
-                if(nums[mid]<=target and target<=nums[high]){
-                    low = mid+1;
+                if(nums[mid]<= target and target <= nums[right]){
+                    left = mid+1;
                 }else{
-                    high = mid-1;
+                    right = mid-1;
                 }
             }
         }
 
         return -1;
-        
     }
 };
