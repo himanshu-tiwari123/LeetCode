387e6f81026d5d4a4defc67101abdd91de08c096
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-05-22 01:27:21 +0530

    Time: 0 ms (100%), Space: 14.5 MB (13.12%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,28 +14,52 @@ class Solution {
 
 
         if(s1[i] == s2[j]){
-            return 0 + findAns(i-1,j-1,s1,s2);
+            return dp[i][j] = 0 + findAns(i-1,j-1,s1,s2);
         }
 
 
         //3 parts: 
-        //Insert
-        return dp[i][j] = 1 + min({findAns(i,j-1,s1,s2) ,findAns(i-1,j,s1,s2),findAns(i-1,j-1,s1,s2) });
-        // int insert = 1 + findAns(i,j-1,s1,s2);
+        
+        // return dp[i][j] = 1 + min({findAns(i,j-1,s1,s2) ,findAns(i-1,j,s1,s2),findAns(i-1,j-1,s1,s2) });
 
-        // int remove = 1 + findAns(i-1,j,s1,s2);
+        int insert = 1 + findAns(i,j-1,s1,s2);
 
-        // int replace = 1 + findAns(i-1,j-1,s1,s2);
+        int remove = 1 + findAns(i-1,j,s1,s2);
+
+        int replace = 1 + findAns(i-1,j-1,s1,s2);
 
 
-        // return dp[i][j] = min({insert,remove,replace});
+        return dp[i][j] = min({insert,remove,replace});
     }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size() , m = word2.size();
 
         dp.assign(n+1,vector<int>(m+1,-1));
-
+       //Using memoization:
         return findAns(n-1,m-1,word1,word2);
+
+        // //Using Tabulation;
+        // dp.assign(n+1,vector<int>(m+1,0));
+
+        // //Base case is if(i==0) return j || if(j==0) return 1;
+        // for(int j=0;j<=m;j++){
+        //     dp[0][j] = j;
+        // }
+
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = i;
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s1[i-1] == s2[j-1]){
+
+        //         }
+        //     }
+        // }
+
+
+
     }
 };
