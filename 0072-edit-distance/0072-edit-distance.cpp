b020a2f3e7b163df40ba8f3d1c8d1161c2be424c
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-05-22 01:30:13 +0530

    Time: 8 ms (47.65%), Space: 13.3 MB (39.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -35,29 +35,39 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size() , m = word2.size();
 
-        dp.assign(n+1,vector<int>(m+1,-1));
-       //Using memoization:
-        return findAns(n-1,m-1,word1,word2);
+    //     dp.assign(n+1,vector<int>(m+1,-1));
+    //    //Using memoization:
+    //     return findAns(n-1,m-1,word1,word2);
 
-        // //Using Tabulation;
-        // dp.assign(n+1,vector<int>(m+1,0));
+        //Using Tabulation;
+        dp.assign(n+1,vector<int>(m+1,0));
 
-        // //Base case is if(i==0) return j || if(j==0) return 1;
-        // for(int j=0;j<=m;j++){
-        //     dp[0][j] = j;
-        // }
+        //Base case is if(i==0) return j || if(j==0) return 1;
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
 
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0] = i;
-        // }
+        for(int i=0;i<=n;i++){
+            dp[i][0] = i;
+        }
 
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //         if(s1[i-1] == s2[j-1]){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int insert = 1 + dp[i][j-1];
 
-        //         }
-        //     }
-        // }
+                    int remove = 1 + dp[i-1][j];
+
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({insert,remove,replace});
+                }
+            }
+        }
+
+        return dp[n][m];
 
 
 
