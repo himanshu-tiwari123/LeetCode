95cf9d68282982a86f9dd9530588635c03334eed
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-05-25 13:27:15 +0530

    Time: 0 ms (100%), Space: 46.2 MB (93.68%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -7,14 +7,14 @@ public:
 
         //We need sum of subarry of length k to be maximum, it's same as saying that subarray of length n-k should be minimum;
 
-        int k1 = (n-k);
+        k = (n-k);
 
         int l=0,r=0,curr_sum = 0, min_sum = INT_MAX ;
 
         while(r<n){
             curr_sum += cardPoints[r];
 
-            if(r-l+1 == k1){
+            if(r-l+1 == k){
                 min_sum = min(min_sum , curr_sum);
                 curr_sum -= cardPoints[l];
                 l++;
