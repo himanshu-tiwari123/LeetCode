ca9bfbce2147b49573f7ebfb70e205a6a442acf9
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-06-09 00:33:33 +0530

    Time: 0 ms (100%), Space: 127.7 MB (88.18%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -3,42 +3,69 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
 
-        vector<int>list1,list2;
+        // vector<int>list1,list2;
 
-        int cnt = 0;
+        // int cnt = 0;
+
+        // for(auto &it: nums){
+        //     if(it < pivot){
+        //         list1.push_back(it);
+        //     }else if(it == pivot){
+        //         cnt++;
+        //     }else{
+        //         list2.push_back(it);
+        //     }
+        // }
+
+        
+
+        // int i=0,j=0;
+
+        // while(i<list1.size()){
+        //     cout<<i<<" ";
+        //     nums[i++] = list1[i];
+        
+        // }
+
+        // while(cnt > 0){
+        //     cout<<i<<" ";
+        //     nums[i++] = pivot;
+        //     cnt--;
+        // }
+
+    
+        // while(j<list2.size()){
+        //     cout<<i<<" ";
+        //    nums[i++] = list2[j++];
+        // }
+
+        // return nums
+
+        //Method 2: 3 pointers:
+        int less_cnt = 0, equal_cnt  = 0;
 
         for(auto &it: nums){
             if(it < pivot){
-                list1.push_back(it);
+                less_cnt ++;
             }else if(it == pivot){
-                cnt++;
-            }else{
-                list2.push_back(it);
+                equal_cnt ++;
             }
         }
 
-        
+        vector<int>ans(n);
 
-        int i=0,j=0;
+        int i=0, j = less_cnt , k = less_cnt + equal_cnt;
 
-        while(i<list1.size()){
-            cout<<i<<" ";
-            nums[i++] = list1[i];
-        
-        }
-
-        while(cnt > 0){
-            cout<<i<<" ";
-            nums[i++] = pivot;
-            cnt--;
-        }
-
-    
-        while(j<list2.size()){
-            cout<<i<<" ";
-           nums[i++] = list2[j++];
+        for(auto &it: nums){
+            if(it < pivot){
+                ans[i++] = it;
+            }else if(it == pivot){
+                ans[j++] = it;
+            }else{
+                ans[k++] = it;
+            }
         }
         
-        return nums;
+        return ans;
     }
 };
