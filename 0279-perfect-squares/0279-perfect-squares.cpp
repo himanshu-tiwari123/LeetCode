8274b95f6cd066e8d8937fe154caf90171c26d32
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-06-30 00:04:10 +0530

    Time: 38 ms (84.74%), Space: 14.1 MB (23.57%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -7,35 +7,48 @@ public:
             perfectSquares.push_back(i*i);
         }
 
-        vector<vector<int>>dp(100,vector<int>(n+1,-1));
+        // vector<vector<int>>dp(100,vector<int>(n+1,-1));
 
-        auto findAns = [&](auto &self, int index, int n)->int{
-            //Base Case:
-            if(n==0){
-                return 0;
+        // auto findAns = [&](auto &self, int index, int n)->int{
+        //     //Base Case:
+        //     if(n==0){
+        //         return 0;
+        //     }
+        //     if(index >= perfectSquares.size()){
+        //         return 1e9;
+        //     }
+
+        //     if(dp[index][n]!=-1){
+        //         return dp[index][n];
+        //     }
+
+        //     int not_take = self(self,index+1,n);
+
+        //     int take = 1e9;               
+
+        //     if(n-perfectSquares[index] >= 0){
+        //         take = 1 + self(self,index, n-perfectSquares[index]);
+        //     }
+
+
+        //     return dp[index][n] = min(take,not_take);
+        // };
+
+        vector<int>dp(n+1,INT_MAX);
+
+        dp[0] = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i] = min(dp[i], 1+dp[i-j*j]);
             }
-            if(index >= perfectSquares.size()){
-                return 1e9;
-            }
-
-            if(dp[index][n]!=-1){
-                return dp[index][n];
-            }
-
-            int not_take = self(self,index+1,n);
-
-            int take = 1e9;
-
-            if(n-perfectSquares[index] >= 0){
-                take = 1 + self(self,index, n-perfectSquares[index]);
-            }
+        }
 
 
-            return dp[index][n] = min(take,not_take);
-        };
+        return dp[n];
 
 
 
-        return findAns(findAns,0,n);
+        // return findAns(findAns,0,n);
     }
 };
