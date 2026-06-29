a0cd67275bcc7f43b96a4bf53bdcf1ca4d4ba541
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-06-29 18:27:50 +0530

    Time: 857 ms (5%), Space: 267 MB (5.02%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,73 +1,41 @@
 class Solution {
-    vector<vector<int>>dp;
-    
-
-    int findAns(int index,int target,vector<int>&coins){
-        //Base Condition:
-        // if(index>=coins.size()){
-        //     return INT_MAX;
-        // }
-
-        if(index ==  coins.size()-1){
-            if((target % coins[index]) == 0){
-                return target/coins[index];
-            }
-
-            return INT_MAX;
-        }
-
-
-        if(dp[index][target]!=-1){
-            return dp[index][target];
-        }
-
-        int not_take = findAns(index+1,target,coins);
-        int take = 1e9;
-
-        if(target >= coins[index]){
-            take =1+findAns(index,target-coins[index],coins);
-        }
-
-        return dp[index][target] = min(take,not_take);
-    }
 public:
     int numSquares(int n) {
-        //similar to coin change man:
-        vector<int>coins;
-        int i=1;
-        while((i*i) <= n){
-            coins.push_back(i*i);
-            i++;
+        vector<int>perfectSquares;
+
+        for(int i=1;i<=100;i++){
+            perfectSquares.push_back(i*i);
         }
 
-        int m = coins.size();
+        vector<vector<int>>dp(100,vector<int>(n+1,-1));
 
-        dp.resize(m+1,vector<int>(n+1,INT_MAX));
-
-        // return findAns(0,n,coins);
-
-        // Lets to tabulation approach:
-        for(int target=0;target<=n;target++){
-            if(target%coins[0] ==0){
-                dp[0][target] = (target/coins[0]);
+        auto findAns = [&](auto &self, int index, int n)->int{
+            //Base Case:
+            if(n==0){
+                return 0;
             }
-        }
-
-        for(int index=1;index<m;index++){
-            for(int target=0;target<=n;target++){
-              int not_take = dp[index-1][target];
-
-              int take = 1e9;
-
-              if(target-coins[index] >=0){
-                take = 1 + dp[index][target-coins[index]];
-              }
-
-              dp[index][target] = min(take,not_take);
-
+            if(index >= perfectSquares.size()){
+                return 1e9;
             }
-        }
 
-        return dp[m-1][n];
+            if(dp[index][n]!=-1){
+                return dp[index][n];
+            }
+
+            int not_take = self(self,index+1,n);
+
+            int take = 1e9;
+
+            if(n-perfectSquares[index] >= 0){
+                take = 1 + self(self,index, n-perfectSquares[index]);
+            }
+
+
+            return dp[index][n] = min(take,not_take);
+        };
+
+
+
+        return findAns(findAns,0,n);
     }
 };
