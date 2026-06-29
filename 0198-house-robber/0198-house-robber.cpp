29601657d07eff61c06ad56b97ce7dcd91fd6cf1
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-06-29 17:41:09 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,27 +2,39 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
+        // vector<int>dp(n+1,-1);
 
-        auto findAns =[&](auto &self, int index)->int{
-            //Base Case:
-            if(index >= n){
-                return 0;
-            }
+        // auto findAns =[&](auto &self, int index)->int{
+        //     //Base Case:
+        //     if(index >= n){
+        //         return 0;
+        //     }
 
-            if(dp[index] != -1){
-                return dp[index];
-            }
+        //     if(dp[index] != -1){
+        //         return dp[index];
+        //     }
 
-            int not_rob = self(self,index+1);
+        //     int not_rob = self(self,index+1);
 
-            int rob = nums[index] + self(self,index+2);
-
-
-            return dp[index] = max(rob, not_rob); 
-        };
+        //     int rob = nums[index] + self(self,index+2);
 
 
-        return findAns(findAns,0);
+        //     return dp[index] = max(rob, not_rob); 
+        // };
+
+        vector<int>dp(n+2,0);
+
+
+        for(int i=n-1;i>=0;i--){
+            dp[i]= max(dp[i+1] , nums[i] + dp[i+2]);
+        }
+
+
+        return dp[0];
+
+
+
+
+        // return findAns(findAns,0);
     }
 };
