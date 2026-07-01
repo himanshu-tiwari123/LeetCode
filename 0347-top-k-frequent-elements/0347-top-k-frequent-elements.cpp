5d3bcfad6c7d88fb13a4f958d47bf88dc8e70752
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-07-01 13:33:50 +0530

    Time: 17 ms (8.5%), Space: 17.9 MB (62.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,39 +1,43 @@
 class Solution {
-    using V = vector<int>;
+    using P = pair<int,int>;
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int  n = nums.size();
-
+        int n = nums.size();
+        vector<int>ans;
         unordered_map<int,int>mp;
+        priority_queue<P,vector<P>,greater<P>>pq;
 
-        for(auto &it: nums){
-            mp[it]++;
+        for(int i=0;i<nums.size();i++){
+           mp[nums[i]]++;
         }
 
-        vector<V>bucket(n+1);
 
         for(auto &it: mp){
-            int val = it.first;
-            int freq = it.second;
-
-            bucket[freq].push_back(val);
-        }
-
-        vector<int>ans;
-
-        for(int i=n;i>=0;i--){
-            if(k<=0) break;
-            if(bucket[i].size() == 0) continue;
-            for(int j=0;j<bucket[i].size() and k>0;j++){
-                ans.push_back(bucket[i][j]);
-                k--;
+           
+            cout<<it.first<<" "<<it.second<<"\n";
+            if(pq.size() >= k){
+                int freq1 = pq.top().first;
+                int freq2 = it.second;
+                if(freq2 >  freq1){
+                    pq.pop();
+                    pq.push({it.second,it.first});
+                }
+            }else{
+                pq.push({it.second, it.first});
             }
+
+            
         }
+
+        while(!pq.empty()){
+            int val = pq.top().second;
+
+            ans.push_back(val);
+
+            pq.pop();
+        }
+
 
         return ans;
-
-
-
-
     }
 };
