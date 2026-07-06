8f89038af43977308e31deb1a3d6e7a20f6bc643
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-07-07 01:43:23 +0530

    Time: 42 ms (62.51%), Space: 14.2 MB (89.21%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int dp[50][50][50][50];
+    int dp[50][50][50];
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
@@ -10,14 +10,15 @@ public:
 
        memset(dp,-1,sizeof(dp));
        
-        auto findAns = [&](auto &self,int r1,int c1,int r2,int c2)->int{
+        auto findAns = [&](auto &self,int r1,int c1,int r2)->int{
             //Base Case:
+            int c2 = (r1+c1 - r2);
             if(r1>=n or c1>=n or r2>=n or c2>=n or grid[r1][c1]==-1 or grid[r2][c2]==-1){
                 return -1e9;
             }
 
-            if(dp[r1][c1][r2][c2] != -1){
-                return dp[r1][c1][r2][c2];
+            if(dp[r1][c1][r2] != -1){
+                return dp[r1][c1][r2];
             }
 
 
@@ -35,14 +36,14 @@ public:
             }
 
 
-            int mov1 = self(self,r1+1,c1,r2+1,c2);
-            int mov2 = self(self,r1,c1+1,r2,c2+1);
-            int mov3 = self(self,r1+1,c1,r2,c2+1);
-            int mov4 = self(self,r1,c1+1,r2+1,c2);
+            int mov1 = self(self,r1+1,c1,r2+1);
+            int mov2 = self(self,r1,c1+1,r2);
+            int mov3 = self(self,r1+1,c1,r2);
+            int mov4 = self(self,r1,c1+1,r2+1);
 
             int max_future_cherries = max({mov1,mov2,mov3,mov4});
 
-            return dp[r1][c1][r2][c2] = curr_cherries + max_future_cherries;
+            return dp[r1][c1][r2] = curr_cherries + max_future_cherries;
 
 
 
@@ -50,7 +51,7 @@ public:
 
 
 
-        return findAns(findAns,0,0,0,0) < 0 ? 0 : findAns(findAns,0,0,0,0) ;
+        return findAns(findAns,0,0,0) < 0 ? 0 : findAns(findAns,0,0,0) ;
 
        
     }
