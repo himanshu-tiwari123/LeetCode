b62ebb008ab5a2b7690846c3b7390e3ec930bfe7
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-07-25 02:34:22 +0530

    Time: 459 ms (36.58%), Space: 11.9 MB (8.6%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,25 +3,23 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n  = board.size() , m = board[0].size();
 
-        auto dfs = [&](auto &dfs, int i,int j,string s, vector<vector<bool>>&visited)->bool{
+        auto dfs = [&](auto &dfs, int i,int j ,int idx, vector<vector<bool>>&visited)->bool{
             if(i<0 || i>=n || j<0 || j>=m) return false;
 
-            if(word[s.size()] != board[i][j]) return false;
+            if(word[idx] != board[i][j]) return false;
 
             if(visited[i][j]) return false;
 
-            s += board[i][j];
-
             visited[i][j] = true;
 
-            if(s == word){
+            if(idx == word.size()-1){
                return true;
             }
 
-            bool ans=dfs(dfs,i+1,j,s,visited) ||
-                    dfs(dfs,i,j+1,s,visited) ||
-                    dfs(dfs,i-1,j,s,visited) ||
-                    dfs(dfs,i,j-1,s,visited);
+            bool ans=dfs(dfs,i+1,j,idx+1,visited) ||
+                    dfs(dfs,i,j+1,idx+1,visited) ||
+                    dfs(dfs,i-1,j,idx+1,visited) ||
+                    dfs(dfs,i,j-1,idx+1,visited);
 
             visited[i][j] = false;
 
@@ -33,7 +31,7 @@ public:
                 string s = "";
                 vector<vector<bool>>visited(n,vector<bool>(m, false));
 
-                if(dfs(dfs,i,j,s,visited)){
+                if(dfs(dfs,i,j,0,visited)){
                     return true;
                 }
             }
