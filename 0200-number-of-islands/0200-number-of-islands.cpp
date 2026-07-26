e89bb37973ca3876bba5231e8f1ae0e390fcbff3
Himanshu Tiwari <158790063+himanshu-tiwari123@users.noreply.github.com>
2026-07-26 17:55:46 +0530

    Time: 22 ms (88.81%), Space: 17.5 MB (43.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,3 +1,11 @@
+static const auto fast_io = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
+
+
 class DSU{
     public:
     vector<int>parent;
